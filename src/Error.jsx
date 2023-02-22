7bcbc3d31dc2ef635fd7531bb99412950f8e4bdb
pawanlaksahan <pawanlaksahan10@gmail.com>
2023-02-23 02:28:19 +0530

    changes to Error.jsx

--- src/Error.jsx
+++ src/Error.jsx
@@ -8,7 +8,7 @@ import "./App.css";
   <header className="container">
     <div class="row justify-content-center mt-3 mb-3">
       <div className="col-8">
-     <h1 className="text-danger">404 Page Not Found</h1>    
+     <h1 className="text-danger">404 -- Page Not Found</h1>    
      
      </div>
     </div>
